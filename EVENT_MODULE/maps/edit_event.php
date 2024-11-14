ffff412b9335ab467c37e76c98be1122c06cf7a9
Realika Nazareta <nazareta_realika@plpasig.edu.ph>
2024-11-15 04:45:26 +0800

    Add files via upload

---
+++ EVENT_MODULE/maps/edit_event.php
@@ -0,0 +1,23 @@
+Pasagutan po lahat ng detalye at ihanda ang ORIGINAL na BLOOD REQUEST FORM ng pasyente na dapat ay nasa bantay na tatangap ng dadaling dugo sa ospital.
+
+Requester:
+Group: 
+Donor:
+SHCIM:
+
+
+PAUNAWA:
+Ang dugo po ay LIBRE na ibabahagi at dadalin ng BLOOD  RIDER ng Serving Hearts Charity Inc. wala pong sinuman tao o grupo ang  maaring humingi ng kahit ano man kabayaran.
+
+Pakiusap po na paki lagyan ng sagot lalo na ang numero ng tatangap sa dadalin na libreng dugo.
+
+Patient's Name: 
+Birthday: 
+Ailment: 
+Hospital:
+Blood Type: 
+Blood Component:
+How many bags:
+Physicians Name or Attending Physicians Name:
+Contact person name & mobile no#:
+Messenger/Viber:
