7ae05652d4671d86454ff218e76b7b4ba67fd07e
Realika Nazareta <nazareta_realika@plpasig.edu.ph>
2024-11-15 07:34:59 +0800

    Update db.php

--- USER-VERIFICATION/config/db.php
+++ USER-VERIFICATION/config/db.php
@@ -1,27 +1,10 @@
 <?php
 
-// Allows encryption of important database passwords
+//Allows encryption of important database passwords
 require 'constants.php';
 
-// Get the DATABASE_URL from Heroku environment variables
-$databaseUrl = getenv('DATABASE_URL');
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-// Parse the DATABASE_URL to extract the components
-$parsedUrl = parse_url($databaseUrl);
-
-$host = $parsedUrl['host'];
-$port = $parsedUrl['port'];
-$username = $parsedUrl['user'];
-$password = $parsedUrl['pass'];
-$dbname = ltrim($parsedUrl['path'], '/');
-
-// Create a connection to the MySQL database
-$conn = new mysqli($host, $username, $password, $dbname, $port);
-
-// Check the connection
-if ($conn->connect_error) {
-    die('Database error: ' . $conn->connect_error);
+if($conn->connect_error) {
+    die('Database error:'. $conn->connect_error);
 }
-
-echo "Database connected successfully!";
-?>
