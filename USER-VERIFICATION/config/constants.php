f3850681c05e474e6a027de9fb232bb5314af827
Realika Nazareta <nazareta_realika@plpasig.edu.ph>
2024-11-15 10:53:03 +0800

    Update constants.php

--- USER-VERIFICATION/config/constants.php
+++ USER-VERIFICATION/config/constants.php
@@ -4,7 +4,7 @@ define('EMAIL','shverified@gmail.com');
 define('PASSWORD', 'fumt fduy biuq emhs');
 
 //Database information and passwords
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'serving-hearts');
+define('DB_HOST', 'vvfv20el7sb2enn3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+define('DB_USER', 'ps1u43eztn9ag7ym');
+define('DB_PASS', 'zxs25finszh1r9uz');
+define('DB_NAME', 'im4lt5tgvscipyvc');
